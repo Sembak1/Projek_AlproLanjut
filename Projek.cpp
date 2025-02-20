031e3f4839fef5f3f41882192c07cba4d6de8691
Sembak1 <faizacer12345@gmail.com>
2025-02-20 21:34:50 +0700

    Add files via upload
    Projek Alpro Lanjut

---
+++ Projek.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+
+using namespace std;
+
+struct Menejemen {
+    string tanggal;
+    string jenis;
+    double jumlah;
+};
+Menejemen keuangan[100];
+int jumlahmasuk = 0;
+
+void masuk(){
+    cout << "|=======================|" << endl;
+    cout << "|   Masukan Transaksi   |" << endl;
+    cout << "|=======================|" << endl;
+    cout << "Masukkan tanggal (DD/MM/YYYY): ";
+    cin >> keuangan[jumlahmasuk].tanggal;
+    cout << "Masukkan jenis (Pemasukan/Pengeluaran): ";
+    cin >> keuangan[jumlahmasuk].jenis;
+    cout << "Masukkan jumlah: ";
+    cin >> keuangan[jumlahmasuk].jumlah;
+    jumlahmasuk++;
+}
+void menu (){
+    cout << "|=======================|" << endl;
+    cout << "|  Menjemen Keuangan    |" << endl;
+    cout << "|=======================|" << endl;
+    cout << "| 1. Masukan Transaksi  |" << endl;
+    cout << "| 2. Lihat Transaksi    |" << endl;
+    cout << "| 3. Lihat Saldo        |" << endl;
+    cout << "| 4. Keluar             |" << endl;
+    cout << "|=======================|" << endl;
+}
+void lihat(){
+    cout << "|=======================|" << endl;
+    cout << "|     Data Keuangan     | " << endl;
+    cout << "|=======================|" << endl;
+    for (int i = 0; i < jumlahmasuk; i++) {
+        cout << "Tanggal: " << keuangan[i].tanggal << endl;
+        cout << "Jenis: " << keuangan[i].jenis << endl;
+        cout << "Jumlah: " << keuangan[i].jumlah << endl;
+    }
+}
+void saldo(){
+    double saldo = 0;
+    for (int i = 0; i < jumlahmasuk; i++) {
+        if (keuangan[i].jenis == "Pemasukan") {
+            saldo += keuangan[i].jumlah;
+        } else {
+            saldo -= keuangan[i].jumlah;
+        }
+    }
+    cout << "|=======================|" << endl;
+    cout << "|      Lihat Saldo      |" << endl;
+    cout << "|=======================|" << endl;
+    cout << "Saldo Anda: " << saldo << endl;
+    
+}
+int main() {
+    char kembali;
+    do {
+        menu();
+        int pilihan;
+        cout << "|Masukkan pilihan: ";
+        cin >> pilihan;
+        system("cls");
+
+        switch (pilihan) {
+            case 1:
+                masuk();
+                break;
+            case 2:
+                lihat();
+                break;
+            case 3:
+                saldo();
+                break;
+            case 4:
+                return 0;
+            default:
+                cout << "Pilihan tidak valid." << endl;
+                break;
+        }
+
+        cout << "Apakah Anda ingin kembali ke menu? (y/n): ";
+        cin >> kembali;
+        system("cls"); 
+    } while (kembali == 'y' || kembali == 'Y');
+
+    return 0;
+}
+
+   
